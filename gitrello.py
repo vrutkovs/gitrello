83a8ba8f22b2eb822decbf760b5c4e2421d3c6f3
Vadim Rutkovsky <vrutkovs@redhat.com>
2018-06-12 01:01:36 +0200

    Make PR search parallel and configure a number of workers

--- gitrello.py
+++ gitrello.py
@@ -8,6 +8,7 @@ from github3 import GitHub
 
 
 DEFAULT_SYNC_TIMEOUT = 60
+DEFAULT_WORKERS = 4
 
 config_path = os.path.abspath('config/gitrello.yml')
 
@@ -26,6 +27,7 @@ async def read_config():
     assert 'lists' in config
 
     config['sync_timeout'] = config.get("sync_timeout") or DEFAULT_SYNC_TIMEOUT
+    config['workers'] = config.get("workers") or DEFAULT_WORKERS
 
     trello_client = TrelloClient(api_key=config['trello_appkey'], token=config['trello_token'])
     board = trello_client.get_board(config['trello_boardid'])
@@ -54,7 +56,7 @@ async def create_missing_lists(config):
     lists_to_remove = existing_list_names - required_list_names
 
     futures = []
-    with concurrent.futures.ThreadPoolExecutor(max_workers=4) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers=config['workers']) as executor:
         for lst in lists_to_add:
             futures.append(loop.run_in_executor(executor, add_list, board, lst))
         for lst in lists_to_remove:
@@ -72,7 +74,7 @@ def close_list(board, trello_lists, lst):
     trello_list = board.get_list(lst_id)
     trello_list.close()
 
-async def prs_to_sync(config, list_name, trello_list):
+def prs_to_sync(config, list_name, trello_list):
     gh = config['github']
     search_query = config['lists'][list_name]
 
@@ -80,24 +82,6 @@ async def prs_to_sync(config, list_name, trello_list):
     prs = dict([x.as_dict()['title'], x.as_dict()] for x in search_results)
     existing_card_names = [x.name for x in trello_list.list_cards()]
     return prs, existing_card_names
-
-async def sync_list(config, list_name, trello_list):
-    prs, existing_card_names = await prs_to_sync(config, list_name, trello_list)
-
-    cards_to_remove = existing_card_names - prs.keys()
-    cards_to_create = prs.keys() - existing_card_names
-
-    print("Found {0} cards to remove and {1} cards to add in '{2}'".format(
-          len(cards_to_remove), len(cards_to_create), list_name))
-
-    for card_name in cards_to_remove:
-        print(f"Removing '{card_name}' card from '{list_name}' list")
-        await remove_task(card_name, trello_list)
-
-    for card_name in cards_to_create:
-        print(f"Adding '{card_name}' card to '{list_name}' list")
-        pr = prs[card_name]
-        await add_task(pr, trello_list)
 
 async def get_lists(config, trello_lists):
     for list_name in config['lists']:
@@ -111,16 +95,56 @@ async def sync(config):
 
     trello_lists = board.list_lists()
 
-    async for list_name, trello_list in get_lists(config, trello_lists):
-        await sync_list(config, list_name, trello_list)
+    # Run searches in parallel
+    search_inputs = []
+    search_futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=config['workers']) as executor:
+        async for list_name, trello_list in get_lists(config, trello_lists):
+            search_input = (list_name, trello_list)
+            search_inputs.append(search_input)
+            search_futures.append(loop.run_in_executor(executor, prs_to_sync, config, *search_input))
 
-async def add_task(pr, trello_list):
+    search_results = await asyncio.gather(*search_futures)
+
+    # Collect search results and run actions in parallel
+    action_args = []
+    for (search_input, result) in zip(search_inputs, search_results):
+        list_name, trello_list = search_input
+        prs, existing_card_names = result
+        cards_to_remove = existing_card_names - prs.keys()
+        cards_to_create = prs.keys() - existing_card_names
+
+        print("Found {0} cards to remove and {1} cards to add in '{2}'".format(
+              len(cards_to_remove), len(cards_to_create), list_name))
+
+        for card_name in cards_to_remove:
+            print(f"Removing '{card_name}' card from '{list_name}' list")
+            action_args.append((remove_task, card_name, trello_list))
+
+        for card_name in cards_to_create:
+            print(f"Adding '{card_name}' card to '{list_name}' list")
+            pr = prs[card_name]
+            action_args.append((add_task, pr, trello_list))
+
+    # No actions to be performed - exit
+    if not action_args:
+        print("No actions to perform")
+        return
+
+    action_futures = []
+    with concurrent.futures.ThreadPoolExecutor(max_workers=config['workers']) as executor:
+        for action_arg in action_args:
+            action_futures.append(loop.run_in_executor(executor, *action_arg))
+
+    await asyncio.gather(*action_futures)
+
+def add_task(pr, trello_list):
     url = pr['html_url']
     title = pr['title']
     card = trello_list.add_card(title)
     card.attach(url=url)
 
-async def remove_task(title, trello_list):
+def remove_task(title, trello_list):
     for card in trello_list.list_cards():
         if card.name == title:
             card.delete()
